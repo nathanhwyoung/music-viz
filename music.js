63d4674548ceb50dee745569551a661b885de017
Nathan <nhwilcox@gmail.com>
2016-03-14 19:44:40 -0700

    init files

---
+++ music.js
@@ -0,0 +1,767 @@
+var musicData = [
+	{
+		"note" : "C0",
+		"frequency" : 16.35,
+		"watelength" : 2109.89
+	},
+	{
+		"note" : "C#0",
+		"frequency" : 17.32,
+		"watelength" : 1991.47
+	},
+	{
+		"note" : "Db0",
+		"frequency" : 17.32,
+		"watelength" : 1991.47
+	},
+	{
+		"note" : "D0",
+		"frequency" : 18.35,
+		"watelength" : 1879.69
+	},
+	{
+		"note" : "D#0",
+		"frequency" : 19.45,
+		"watelength" : 1774.20
+	},
+	{
+		"note" : "Eb0",
+		"frequency" : 19.45,
+		"watelength" : 1774.20
+	},
+	{
+		"note" : "E0",
+		"frequency" : 20.60,
+		"watelength" : 1674.62
+	},
+	{
+		"note" : "F0",
+		"frequency" : 21.83,
+		"watelength" : 1580.63
+	},
+	{
+		"note" : "F#0",
+		"frequency" : 23.12,
+		"watelength" : 1491.91
+	},
+	{
+		"note" : "Gb0",
+		"frequency" : 23.12,
+		"watelength" : 1491.91
+	},
+	{
+		"note" : "G0",
+		"frequency" : 24.50,
+		"watelength" : 1408.18
+	},
+	{
+		"note" : "G#0",
+		"frequency" : 25.96,
+		"watelength" : 1329.14
+	},
+	{
+		"note" : "Ab0",
+		"frequency" : 25.96,
+		"watelength" : 1329.14
+	},
+	{
+		"note" : "A0",
+		"frequency" : 27.50,
+		"watelength" : 1254.55
+	},
+	{
+		"note" : "A#0",
+		"frequency" : 29.14,
+		"watelength" : 1184.13
+	},
+	{
+		"note" : "Bb0",
+		"frequency" : 29.14,
+		"watelength" : 1184.13
+	},
+	{
+		"note" : "B0",
+		"frequency" : 30.87,
+		"watelength" : 1117.67
+	},
+	{
+		"note" : "C1",
+		"frequency" : 32.70,
+		"watelength" : 1054.94
+	},
+	{
+		"note" : "C#1",
+		"frequency" : 34.65,
+		"watelength" : 995.73
+	},
+	{
+		"note" : "Db1",
+		"frequency" : 34.65,
+		"watelength" : 995.73
+	},
+	{
+		"note" : "D1",
+		"frequency" : 36.71,
+		"watelength" : 939.85
+	},
+	{
+		"note" : "D#1",
+		"frequency" : 38.89,
+		"watelength" : 887.10
+	},
+	{
+		"note" : "Eb1",
+		"frequency" : 38.89,
+		"watelength" : 887.10
+	},
+	{
+		"note" : "E1",
+		"frequency" : 41.20,
+		"watelength" : 837.31
+	},
+	{
+		"note" : "F1",
+		"frequency" : 43.65,
+		"watelength" : 790.31
+	},
+	{
+		"note" : "F#1",
+		"frequency" : 46.25,
+		"watelength" : 745.96
+	},
+	{
+		"note" : "Gb1",
+		"frequency" : 46.25,
+		"watelength" : 745.96
+	},
+	{
+		"note" : "G1",
+		"frequency" : 49.00,
+		"watelength" : 704.09
+	},
+	{
+		"note" : "G#1",
+		"frequency" : 51.91,
+		"watelength" : 664.57
+	},
+	{
+		"note" : "Ab1",
+		"frequency" : 51.91,
+		"watelength" : 664.57
+	},
+	{
+		"note" : "A1",
+		"frequency" : 55.00,
+		"watelength" : 627.27
+	},
+	{
+		"note" : "A#1",
+		"frequency" : 58.27,
+		"watelength" : 592.07
+	},
+	{
+		"note" : "Bb1",
+		"frequency" : 58.27,
+		"watelength" : 592.07
+	},
+	{
+		"note" : "B1",
+		"frequency" : 61.74,
+		"watelength" : 558.84
+	},
+	{
+		"note" : "C2",
+		"frequency" : 65.41,
+		"watelength" : 527.47
+	},
+	{
+		"note" : "C#2",
+		"frequency" : 69.30,
+		"watelength" : 497.87
+	},
+	{
+		"note" : "Db2",
+		"frequency" : 69.30,
+		"watelength" : 497.87
+	},
+	{
+		"note" : "D2",
+		"frequency" : 73.42,
+		"watelength" : 469.92
+	},
+	{
+		"note" : "D#2",
+		"frequency" : 77.78,
+		"watelength" : 443.55
+	},
+	{
+		"note" : "Eb2",
+		"frequency" : 77.78,
+		"watelength" : 443.55
+	},
+	{
+		"note" : "E2",
+		"frequency" : 82.41,
+		"watelength" : 418.65
+	},
+	{
+		"note" : "F2",
+		"frequency" : 87.31,
+		"watelength" : 395.16
+	},
+	{
+		"note" : "F#2",
+		"frequency" : 92.50,
+		"watelength" : 372.98
+	},
+	{
+		"note" : "Gb2",
+		"frequency" : 92.50,
+		"watelength" : 372.98
+	},
+	{
+		"note" : "G2",
+		"frequency" : 98.00,
+		"watelength" : 352.04
+	},
+	{
+		"note" : "G#2",
+		"frequency" : 103.83,
+		"watelength" : 332.29
+	},
+	{
+		"note" : "Ab2",
+		"frequency" : 103.83,
+		"watelength" : 332.29
+	},
+	{
+		"note" : "A2",
+		"frequency" : 110.00,
+		"watelength" : 313.64
+	},
+	{
+		"note" : "A#2",
+		"frequency" : 116.54,
+		"watelength" : 296.03
+	},
+	{
+		"note" : "Bb2",
+		"frequency" : 116.54,
+		"watelength" : 296.03
+	},
+	{
+		"note" : "B2",
+		"frequency" : 123.47,
+		"watelength" : 279.42
+	},
+	{
+		"note" : "C3",
+		"frequency" : 130.81,
+		"watelength" : 263.74
+	},
+	{
+		"note" : "C#3",
+		"frequency" : 138.59,
+		"watelength" : 248.93
+	},
+	{
+		"note" : "Db3",
+		"frequency" : 138.59,
+		"watelength" : 248.93
+	},
+	{
+		"note" : "D3",
+		"frequency" : 146.83,
+		"watelength" : 234.96
+	},
+	{
+		"note" : "D#3",
+		"frequency" : 155.56,
+		"watelength" : 221.77
+	},
+	{
+		"note" : "Eb3",
+		"frequency" : 155.56,
+		"watelength" : 221.77
+	},
+	{
+		"note" : "E3",
+		"frequency" : 164.81,
+		"watelength" : 209.33
+	},
+	{
+		"note" : "F3",
+		"frequency" : 174.61,
+		"watelength" : 197.58
+	},
+	{
+		"note" : "F#3",
+		"frequency" : 185.00,
+		"watelength" : 186.49
+	},
+	{
+		"note" : "Gb3",
+		"frequency" : 185.00,
+		"watelength" : 186.49
+	},
+	{
+		"note" : "G3",
+		"frequency" : 196.00,
+		"watelength" : 176.02
+	},
+	{
+		"note" : "G#3",
+		"frequency" : 207.65,
+		"watelength" : 166.14
+	},
+	{
+		"note" : "Ab3",
+		"frequency" : 207.65,
+		"watelength" : 166.14
+	},
+	{
+		"note" : "A3",
+		"frequency" : 220.00,
+		"watelength" : 156.82
+	},
+	{
+		"note" : "A#3",
+		"frequency" : 233.08,
+		"watelength" : 148.02
+	},
+	{
+		"note" : "Bb3",
+		"frequency" : 233.08,
+		"watelength" : 148.02
+	},
+	{
+		"note" : "B3",
+		"frequency" : 246.94,
+		"watelength" : 139.71
+	},
+	{
+		"note" : "C4",
+		"frequency" : 261.63,
+		"watelength" : 131.87
+	},
+	{
+		"note" : "C#4",
+		"frequency" : 277.18,
+		"watelength" : 124.47
+	},
+	{
+		"note" : "Db4",
+		"frequency" : 277.18,
+		"watelength" : 124.47
+	},
+	{
+		"note" : "D4",
+		"frequency" : 293.66,
+		"watelength" : 117.48
+	},
+	{
+		"note" : "D#4",
+		"frequency" : 311.13,
+		"watelength" : 110.89
+	},
+	{
+		"note" : "Eb4",
+		"frequency" : 311.13,
+		"watelength" : 110.89
+	},
+	{
+		"note" : "E4",
+		"frequency" : 329.63,
+		"watelength" : 104.66
+	},
+	{
+		"note" : "F4",
+		"frequency" : 349.23,
+		"watelength" : 98.79
+	},
+	{
+		"note" : "F#4",
+		"frequency" : 369.99,
+		"watelength" : 93.24
+	},
+	{
+		"note" : "Gb4",
+		"frequency" : 369.99,
+		"watelength" : 93.24
+	},
+	{
+		"note" : "G4",
+		"frequency" : 392.00,
+		"watelength" : 88.01
+	},
+	{
+		"note" : "G#4",
+		"frequency" : 415.30,
+		"watelength" : 83.07
+	},
+	{
+		"note" : "Ab4",
+		"frequency" : 415.30,
+		"watelength" : 83.07
+	},
+	{
+		"note" : "A4",
+		"frequency" : 440.00,
+		"watelength" : 78.41
+	},
+	{
+		"note" : "A#4",
+		"frequency" : 466.16,
+		"watelength" : 74.01
+	},
+	{
+		"note" : "Bb4",
+		"frequency" : 466.16,
+		"watelength" : 74.01
+	},
+	{
+		"note" : "B4",
+		"frequency" : 493.88,
+		"watelength" : 69.85
+	},
+	{
+		"note" : "C5",
+		"frequency" : 523.25,
+		"watelength" : 65.93
+	},
+	{
+		"note" : "C#5",
+		"frequency" : 554.37,
+		"watelength" : 62.23
+	},
+	{
+		"note" : "Db5",
+		"frequency" : 554.37,
+		"watelength" : 62.23
+	},
+	{
+		"note" : "D5",
+		"frequency" : 587.33,
+		"watelength" : 58.74
+	},
+	{
+		"note" : "D#5",
+		"frequency" : 622.25,
+		"watelength" : 55.44
+	},
+	{
+		"note" : "Eb5",
+		"frequency" : 622.25,
+		"watelength" : 55.44
+	},
+	{
+		"note" : "E5",
+		"frequency" : 659.25,
+		"watelength" : 52.33
+	},
+	{
+		"note" : "F5",
+		"frequency" : 698.46,
+		"watelength" : 49.39
+	},
+	{
+		"note" : "F#5",
+		"frequency" : 739.99,
+		"watelength" : 46.62
+	},
+	{
+		"note" : "Gb5",
+		"frequency" : 739.99,
+		"watelength" : 46.62
+	},
+	{
+		"note" : "G5",
+		"frequency" : 783.99,
+		"watelength" : 44.01
+	},
+	{
+		"note" : "G#5",
+		"frequency" : 830.61,
+		"watelength" : 41.54
+	},
+	{
+		"note" : "Ab5",
+		"frequency" : 830.61,
+		"watelength" : 41.54
+	},
+	{
+		"note" : "A5",
+		"frequency" : 880.00,
+		"watelength" : 39.20
+	},
+	{
+		"note" : "A#5",
+		"frequency" : 932.33,
+		"watelength" : 37.00
+	},
+	{
+		"note" : "Bb5",
+		"frequency" : 932.33,
+		"watelength" : 37.00
+	},
+	{
+		"note" : "B5",
+		"frequency" : 987.77,
+		"watelength" : 34.93
+	},
+	{
+		"note" : "C6",
+		"frequency" : 1046.50,
+		"watelength" : 32.97
+	},
+	{
+		"note" : "C#6",
+		"frequency" : 1108.73,
+		"watelength" : 31.12
+	},
+	{
+		"note" : "Db6",
+		"frequency" : 1108.73,
+		"watelength" : 31.12
+	},
+	{
+		"note" : "D6",
+		"frequency" : 1174.66,
+		"watelength" : 29.37
+	},
+	{
+		"note" : "D#6",
+		"frequency" : 1244.51,
+		"watelength" : 27.72
+	},
+	{
+		"note" : "Eb6",
+		"frequency" : 1244.51,
+		"watelength" : 27.72
+	},
+	{
+		"note" : "E6",
+		"frequency" : 1318.51,
+		"watelength" : 26.17
+	},
+	{
+		"note" : "F6",
+		"frequency" : 1396.91,
+		"watelength" : 24.70
+	},
+	{
+		"note" : "F#6",
+		"frequency" : 1479.98,
+		"watelength" : 23.31
+	},
+	{
+		"note" : "Gb6",
+		"frequency" : 1479.98,
+		"watelength" : 23.31
+	},
+	{
+		"note" : "G6",
+		"frequency" : 1567.98,
+		"watelength" : 22.00
+	},
+	{
+		"note" : "G#6",
+		"frequency" : 1661.22,
+		"watelength" : 20.77
+	},
+	{
+		"note" : "Ab6",
+		"frequency" : 1661.22,
+		"watelength" : 20.77
+	},
+	{
+		"note" : "A6",
+		"frequency" : 1760.00,
+		"watelength" : 19.60
+	},
+	{
+		"note" : "A#6",
+		"frequency" : 1864.66,
+		"watelength" : 18.50
+	},
+	{
+		"note" : "Bb6",
+		"frequency" : 1864.66,
+		"watelength" : 18.50
+	},
+	{
+		"note" : "B6",
+		"frequency" : 1975.53,
+		"watelength" : 17.46
+	},
+	{
+		"note" : "C7",
+		"frequency" : 2093.00,
+		"watelength" : 16.48
+	},
+	{
+		"note" : "C#7",
+		"frequency" : 2217.46,
+		"watelength" : 15.56
+	},
+	{
+		"note" : "Db7",
+		"frequency" : 2217.46,
+		"watelength" : 15.56
+	},
+	{
+		"note" : "D7",
+		"frequency" : 2349.32,
+		"watelength" : 14.69
+	},
+	{
+		"note" : "D#7",
+		"frequency" : 2489.02,
+		"watelength" : 13.86
+	},
+	{
+		"note" : "Eb7",
+		"frequency" : 2489.02,
+		"watelength" : 13.86
+	},
+	{
+		"note" : "E7",
+		"frequency" : 2637.02,
+		"watelength" : 13.08
+	},
+	{
+		"note" : "F7",
+		"frequency" : 2793.83,
+		"watelength" : 12.35
+	},
+	{
+		"note" : "F#7",
+		"frequency" : 2959.96,
+		"watelength" : 11.66
+	},
+	{
+		"note" : "Gb7",
+		"frequency" : 2959.96,
+		"watelength" : 11.66
+	},
+	{
+		"note" : "G7",
+		"frequency" : 3135.96,
+		"watelength" : 11.00
+	},
+	{
+		"note" : "G#7",
+		"frequency" : 3322.44,
+		"watelength" : 10.38
+	},
+	{
+		"note" : "Ab7",
+		"frequency" : 3322.44,
+		"watelength" : 10.38
+	},
+	{
+		"note" : "A7",
+		"frequency" : 3520.00,
+		"watelength" : 9.80
+	},
+	{
+		"note" : "A#7",
+		"frequency" : 3729.31,
+		"watelength" : 9.25
+	},
+	{
+		"note" : "Bb7",
+		"frequency" : 3729.31,
+		"watelength" : 9.25
+	},
+	{
+		"note" : "B7",
+		"frequency" : 3951.07,
+		"watelength" : 8.73
+	},
+	{
+		"note" : "C8",
+		"frequency" : 4186.01,
+		"watelength" : 8.24
+	},
+	{
+		"note" : "C#8",
+		"frequency" : 4434.92,
+		"watelength" : 7.78
+	},
+	{
+		"note" : "Db8",
+		"frequency" : 4434.92,
+		"watelength" : 7.78
+	},
+	{
+		"note" : "D8",
+		"frequency" : 4698.63,
+		"watelength" : 7.34
+	},
+	{
+		"note" : "D#8",
+		"frequency" : 4978.03,
+		"watelength" : 6.93
+	},
+	{
+		"note" : "Eb8",
+		"frequency" : 4978.03,
+		"watelength" : 6.93
+	},
+	{
+		"note" : "E8",
+		"frequency" : 5274.04,
+		"watelength" : 6.54
+	},
+	{
+		"note" : "F8",
+		"frequency" : 5587.65,
+		"watelength" : 6.17
+	},
+	{
+		"note" : "F#8",
+		"frequency" : 5919.91,
+		"watelength" : 5.83
+	},
+	{
+		"note" : "Gb8",
+		"frequency" : 5919.91,
+		"watelength" : 5.83
+	},
+	{
+		"note" : "G8",
+		"frequency" : 6271.93,
+		"watelength" : 5.50
+	},
+	{
+		"note" : "G#8",
+		"frequency" : 6644.88,
+		"watelength" : 5.19
+	},
+	{
+		"note" : "Ab8",
+		"frequency" : 6644.88,
+		"watelength" : 5.19
+	},
+	{
+		"note" : "A8",
+		"frequency" : 7040.00,
+		"watelength" : 4.90
+	},
+	{
+		"note" : "A#8",
+		"frequency" : 7458.62,
+		"watelength" : 4.63
+	},
+	{
+		"note" : "Bb8",
+		"frequency" : 7458.62,
+		"watelength" : 4.63
+	},
+	{
+		"note" : "B8",
+		"frequency" : 7902.13,
+		"watelength" : 4.37
+	}
+]
